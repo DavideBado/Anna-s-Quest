00065004e5c6fb27a1506f3ce241bce833bbb899
DavideBado <dabado2510@gmail.com>
2019-01-24 12:45:14 +0100

    Ora elimina le note e aggiunge i punti

--- Assets/ButtonNote.cs
+++ Assets/ButtonNote.cs
@@ -6,24 +6,22 @@ public class ButtonNote : MonoBehaviour
 {
 
     public bool IsTrigger;
-    public Note Note;
+    public Note m_note;
     public GameManager GameManager;
-
+    public GameObject ButtonNote_Note;
 
     private void Awake()
     {
-        GameManager = FindObjectOfType<GameManager>();
+        GameManager = GetComponent<GameManager>();
     }
 
-    private void OnTriggerEnter(Collider other) // ????? Forse questo script è quello mancante nelle note? Ma allora perché avere due script e richiamare il secondo qui?
-    {
-        if (GameManager.ButtonNoteManager == null // ??????? Se sono qui come è possibile che la lista sia == null?
-            && Note == null 
-            && other.tag == "NoteTrigger")
+    private void OnTriggerEnter(Collider other)
+     {
+        if (other.tag == "NoteTrigger")
         {
-           // GameManager.ButtonNote = this;
-            Note = other.gameObject.GetComponent<Note>(); // Quindi questo script è pensato per andare nel trigger della barra nera?
-            GameManager.Note = Note;
+            IsTrigger = true;
+            Debug.Log("Qualcosa funzona");
+            ButtonNote_Note = other.gameObject;
         }
     }
 
@@ -34,6 +32,6 @@ public class ButtonNote : MonoBehaviour
         {
             IsTrigger = true;
         }
-
     }
-}
+   
+    }
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,45 +7,56 @@ public class GameManager : MonoBehaviour
     public int Score;
     public List<ButtonNote> ButtonNoteManager = new List<ButtonNote>();
     public Note Note;
+    ButtonNote A_ButtonNote, S_ButtonNote, D_ButtonNote;
+    GameObject CosoNero;
 
     // Use this for initialization
     void Start()
     {
-
+        A_ButtonNote = GameObject.Find("Trigger A").GetComponent<ButtonNote>();
+        S_ButtonNote = GameObject.Find("Trigger S").GetComponent<ButtonNote>();
+        D_ButtonNote = GameObject.Find("Trigger D").GetComponent<ButtonNote>();
     }
 
     // Update is called once per frame
     void Update()
     {
         CheckInput();
+        Debug.Log(Score);
     }
     void CheckInput()
     {
+        if (Input.GetKeyDown(KeyCode.A) && A_ButtonNote.IsTrigger == true)
 
-        if (Input.GetKeyDown(KeyCode.A) && ButtonNoteManager[0].IsTrigger == true)
-            // Quando abbiamo aggiunto qualcosa alla lista? Siamo sicuri che 0 sia l'oggetto di sinistra? 
-            // Che poi neanche abbiamo una transform legata a ButtonNote quindi sinistra di cosa?
         {
             AddScore();
-            ButtonNoteManager[0].Note.DestroyObject();
+            Destroy(A_ButtonNote.ButtonNote_Note);
+            A_ButtonNote.IsTrigger = false;
+
         }
 
-        if (Input.GetKey(KeyCode.S) && ButtonNoteManager[1].IsTrigger == true)
+        if (Input.GetKeyDown(KeyCode.S) && S_ButtonNote.IsTrigger == true)
+           
         {
-
+            AddScore();
+            Destroy(S_ButtonNote.ButtonNote_Note);
+            S_ButtonNote.IsTrigger = false;
+                        
         }
+        if (Input.GetKeyDown(KeyCode.D) && D_ButtonNote.IsTrigger == true)
 
-        if (Input.GetKey(KeyCode.D) && ButtonNoteManager[2].IsTrigger == true)
         {
+            AddScore();
+            Destroy(D_ButtonNote.ButtonNote_Note);
+            D_ButtonNote.IsTrigger = false;
 
         }
-
     }
 
     public void AddScore()
     {
         Score += 100;
-        Debug.Log(Score);
+     
     }
 
     
